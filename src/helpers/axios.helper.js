38f9214a7d615a9977843b11fa743dac35dbace6
chandanpolai <chandanpolai26@gmail.com>
2024-08-20 17:42:14 +0530

    intial-commit-by-v0

---
+++ src/helpers/axios.helper.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+// import { loadProgressBar } from "axios-progress-bar";
+// import "axios-progress-bar/dist/nprogress.css";
+
+
+const baseURL = "https://uchiha-backend.onrender.com/api/v1";
+
+export const axiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+// loadProgressBar({}, axiosInstance);
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://uchiha-backend.onrender.com/api/v1",
+    },
+  },
+});
+
